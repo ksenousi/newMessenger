92c1414c7e48124d282bacb4a9425b49a3186463
Karim <ksenousi@gmail.com>
2017-06-02 01:04:07 +0100

    added all remaining files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ChatListComponent } from './content/chat-list/chat-list.component';
+import { ChatRoomComponent } from './content/chat-room/chat-room.component';
+import { ChatBarComponent } from './content/chat-room/chat-bar/chat-bar.component';
+import {ContentComponent} from './content/content.component';
+import { MessageComponent } from './content/chat-room/message/message.component';
+import { ChatItemComponent } from './content/chat-list/chat-item/chat-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ChatListComponent,
+    ChatRoomComponent,
+    ChatBarComponent,
+    ContentComponent,
+    MessageComponent,
+    ChatItemComponent
   ],
   imports: [
     BrowserModule,
