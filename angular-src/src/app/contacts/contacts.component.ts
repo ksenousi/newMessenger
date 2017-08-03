c9e4a83367b7114a0cc75777ce84d90f98d0ef51
ksenousi <ksenousi@gmail.com>
2017-08-03 20:58:52 +0100

    add contacts interface

--- angular-src/src/app/contacts/contacts.component.ts
+++ angular-src/src/app/contacts/contacts.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
+  searchCriteria: String;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    
   }
 
 }
