ad591b2b2859de30f6067a4e14566b597659f9a5
Karim <ksenousi@gmail.com>
2017-08-07 16:35:35 +0100

    chat list now populates and is clickable

--- angular-src/src/app/dashboard/chat-list/chat-item/chat-item.component.ts
+++ angular-src/src/app/dashboard/chat-list/chat-item/chat-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chat-item',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatItemComponent implements OnInit {
 
+  @Input() chatName: string
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChatSelected(){
+    console.log(this.chatName +"selected");
   }
 
 }
--- angular-src/src/app/dashboard/chat-list/chat-list.component.ts
+++ angular-src/src/app/dashboard/chat-list/chat-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-chat-list',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatListComponent implements OnInit {
   chats = [1, 1, 1];
-  constructor() { }
+  
+  constructor(private authService: AuthService) {
+
+    this.authService.getProfile().subscribe(profile => {
+      this.chats = profile.user.contacts;
+      console.log(this.chats);
+     },
+     err => {
+      console.log(err);
+      return false;
+    });
+
+  }
 
   ngOnInit() {
   }
