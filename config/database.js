06833c62a33fce3cde99f3bb7af7798fe9cc6148
ksenousi <ksenousi@gmail.com>
2017-08-12 16:01:23 +0100

    added mongo server link

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/messenger',
+    database: 'mongodb://karim:sPongebob234@ds055782.mlab.com:55782/messenger',
     secret: 'yoursecret'
 }
