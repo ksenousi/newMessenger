226df5fed13ea1dfca54592b5dd6177660a307a0
ksenousi <ksenousi@gmail.com>
2017-08-16 21:00:56 +0100

    removed unnecessary fields from db

--- models/chat.js
+++ models/chat.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const config = require('../config/database');
-const UserSchema = require('./user');
 
 // chat schema
 const ChatSchema = mongoose.Schema({
@@ -20,9 +17,6 @@ const ChatSchema = mongoose.Schema({
   messages: [{
     content: String,
     outgoing: Boolean,
-    timeSent: String,
-    timeReceived: String,
-    timeSeen: String
   }]
     
 });
