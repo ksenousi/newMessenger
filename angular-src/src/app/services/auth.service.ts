eb354f0402c874efabb171c8f598e4ca3a832bb1
ksenousi <ksenousi@gmail.com>
2017-08-18 18:19:27 +0100

    changed service to deployment mode

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -9,7 +9,7 @@ export class AuthService {
   user: any;
   isDev: boolean;
   constructor(private http: Http) {
-    this.isDev = true; // Change to false before deployment
+    this.isDev = false; // Change to false before deployment
   }
 
   registerUser(user) {
