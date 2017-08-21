738ca4b5545a99df32b84e9a8cedf887c22781c0
ksenousi <ksenousi@gmail.com>
2017-08-21 18:27:09 +0100

    improved contacts page

--- angular-src/src/app/contacts/search-contacts/search-contacts.component.ts
+++ angular-src/src/app/contacts/search-contacts/search-contacts.component.ts
@@ -36,25 +36,27 @@ export class SearchContactsComponent implements OnInit {
       }
 
       this.results = users;
+      console.log(JSON.stringify(users))
     });
   }
 
-  addRequest(contact: string) {
-    this.authService.addContactRequest({ 'contact': contact }).subscribe(status => {
+  addRequest(contact) {
+    this.authService.addContactRequest({ 'contact': contact.username }).subscribe(status => {
       if (status.success == true) {
-        this.flashMessage.show('Contact has been added', { cssClass: 'alert-success', timeout: 3000 });
+        this.flashMessage.show('Contact request has been sent', { cssClass: 'alert-success', timeout: 3000 });
+        contact.type = 'requestSent';
 
       } else {
         this.flashMessage.show('Failed to add contact', { cssClass: 'alert-danger', timeout: 3000 });
       }
     });
-    this.results = [];
   }
 
-  removeContact(contact: string) {
-    this.authService.removeContact({ 'contact': contact }).subscribe(status => {
+  removeContact(contact) {
+    this.authService.removeContact({ 'contact': contact.username }).subscribe(status => {
       if (status.success == true) {
         this.flashMessage.show('Contact has been removed', { cssClass: 'alert-success', timeout: 3000 });
+        contact.type = 'newUser';
 
       } else {
         this.flashMessage.show('Failed to remove contact', { cssClass: 'alert-danger', timeout: 3000 });
@@ -65,6 +67,4 @@ export class SearchContactsComponent implements OnInit {
   isContact(contact: string) {
     return this.contacts.indexOf(contact) > -1;
   }
-
-
 }
