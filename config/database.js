a548d64eceb24a9efb1d548348081a9d1fe98e34
ksenousi <ksenousi@gmail.com>
2018-09-17 22:44:06 +0100

    config update

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://karim:sPongebob234@ds055782.mlab.com:55782/messenger',
+  database: 'mongodb://heroku:ONlDNckS3mG0@ds055782.mlab.com:55782/messenger',
   secret: 'yoursecret',
 };
